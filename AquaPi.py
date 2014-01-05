55698a28db806e248a7e444a7eb2f974e87a246d
Ian Young <ian.young@me.com>
2014-01-05 12:08:53 -0600

    Stubbing out some methods for future code.

--- AquaPi.py
+++ AquaPi.py
@@ -10,15 +10,21 @@
 # Bring in the main aquapi class
 import aquapi
 
-
-def main():
-	
-	# Launch the test method in the main class
-	aquapi.test('0.0.0.0', 80)
+def daemonize():
+	# setup pidfile and all that crap
+	print 'Daemonize'
 
 def read_config(configfile):
 	print 'Reading configuration file'
 	# Do stuff
+	
+def help_message():
+	print 'Help'
+	# Will make this more verbose later
+	
+def main():
+	# Launch the test method in the main class
+	aquapi.test('0.0.0.0', 80)
 
 # as long as this script was launched directly
 if __name__ == "__main__":
