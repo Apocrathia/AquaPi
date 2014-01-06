fdd83ffa5cb3ef30d2363d68a24bb675e6f929c6
Ian Young <ian.young@me.com>
2014-01-05 19:31:37 -0600

    Writing in some comments about an issue that I'm having. I need figure out how to handle a global arduino object that can be passed around between classes and access it. The problem with what I'm doing right now is that a new object is initialized before it's called to so that the object has a scope that's locally accessible. This causes a huge delay because the initialization of the connection takes a second, and then the message gets pushed. Once a connection is made, it's instantaneous, though. The library that I'm using is awesome about that. I'm just using it in a really dumb way because I'm not a great programmer.

--- aquapi/__init__.py
+++ aquapi/__init__.py
@@ -24,13 +24,26 @@ class View(flask.views.MethodView):
 		
 	def post(self):
 		# we want to execute the code
+		# if the on button was pushed
 		if flask.request.form['btn'] == 'On':
+			# flash the message in the page
 			flask.flash("Turning the lights On")
+			# Right here is an issue: this re-establishes the connection
+			# to the arduino. It takes a second and it's really inefficient.
+			# The problem that I'm having is passing an arduino object with
+			# and already open connection. Not sure how to get it into this class
+			# or if I should just make it global. If anyone reading this has any
+			# suggestions, please let me know.
 			arduino = Arduino('9600')
+			# and turn the pin on
 			arduino.digitalWrite(13, "HIGH")
+		# or if the off button was pushed
 		elif flask.request.form['btn'] == 'Off':
+			# show a message
 			flask.flash("Turning the lights Off")
+			# connect to the arduino
 			arduino = Arduino('9600')
+			# turn the pin off
 			arduino.digitalWrite(13, "LOW")
 		# finally
 		return self.get()
