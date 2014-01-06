a1d1655eb9e2e735acd396b59d7dfffe8bc46d28
Ian Young <ian.young@me.com>
2014-01-06 15:48:33 -0600

    Let's try using a global object and make the controller class a subclass. This will allow me to override anything in polymorphic code. Pushing from this system, pull down to the Pi, and testing next.

--- aquapi/__init__.py
+++ aquapi/__init__.py
@@ -9,32 +9,26 @@ import os
 import flask, flask.views
 
 # import arduino class
-#from controller import controller
+from controller import controller
 # Import arduino API
-from Arduino import Arduino
+#from Arduino import Arduino
 
 # need an object that can be referenced from everywhere
 global arduino
+arduino = controller('9600')
 
 # overloads Flask's view method
 class View(flask.views.MethodView):
 	def get(self):
 		# returns test.html in the 'templates' folder
 		return flask.render_template('test.html')
-		
+
 	def post(self):
 		# we want to execute the code
 		# if the on button was pushed
 		if flask.request.form['btn'] == 'On':
 			# flash the message in the page
 			flask.flash("Turning the lights On")
-			# Right here is an issue: this re-establishes the connection
-			# to the arduino. It takes a second and it's really inefficient.
-			# The problem that I'm having is passing an arduino object with
-			# and already open connection. Not sure how to get it into this class
-			# or if I should just make it global. If anyone reading this has any
-			# suggestions, please let me know.
-			arduino = Arduino('9600')
 			# and turn the pin on
 			arduino.digitalWrite(13, "HIGH")
 		# or if the off button was pushed
@@ -42,29 +36,24 @@ class View(flask.views.MethodView):
 			# show a message
 			flask.flash("Turning the lights Off")
 			# connect to the arduino
-			arduino = Arduino('9600')
-			# turn the pin off
 			arduino.digitalWrite(13, "LOW")
 		# finally
 		return self.get()
 
 def test(host="127.0.0.1", port=8080):
-	# instantiate a new arduino
-	arduino = Arduino('9600')
-	
 	# set pin 13 to output, since that's the one we're using in the test
 	arduino.pinMode(13, "OUTPUT")
 
 	# set a webui object
 	webui = flask.Flask(__name__)
-	
+
 	# need a key for posts
 	webui.secret_key = "aquapi"
-	
+
 	# set the main page
 	webui.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
-	
+
 	webui.debug = True
-	
+
 	# start the server.
 	webui.run(host, port)
--- aquapi/controller.py
+++ aquapi/controller.py
@@ -6,24 +6,24 @@ from Arduino import Arduino
 # needed for some of the sleep functions
 import time
 
-global controller
+class controller(Arduino):
+	def __init__(self):
+		super(controller, self).__init__()
+		#controller.connect('')
 
-def __init__():
-	controller.connect('')
+	def connect(baud='9600'):
+		controller = Arduino(baud)
 
-def connect(baud='9600'):
-	controller = Arduino(baud)
-	
-def initalize(pin, mode='OUTPUT'):
-	controller.pinMode(pin, mode)
+	def initalizeController(pin, mode='OUTPUT'):
+		controller.pinMode(pin, mode)
 
-# blink an LED
-def blink():
-    while True:
-        controller.digitalWrite(led_pin, "HIGH")
-        print controller.digitalRead(led_pin)  # confirm HIGH (1)
-        time.sleep(1)
-        controller.digitalWrite(led_pin, "LOW")
-        print controller.digitalRead(led_pin)  # confirm HIGH (0)
-        time.sleep(1)
+	# blink an LED
+	def blink():
+		while True:
+			controller.digitalWrite(led_pin, "HIGH")
+			print controller.digitalRead(led_pin)  # confirm HIGH (1)
+			time.sleep(1)
+			controller.digitalWrite(led_pin, "LOW")
+			print controller.digitalRead(led_pin)  # confirm HIGH (0)
+			time.sleep(1)
 
